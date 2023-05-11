149f669931437cbcd498cc882394548c55e58649
Markus Riester <markus.riester@novartis.com>
2023-05-11 14:32:59 -0400

    Convert Seurat to Giotto object.

--- R/as.R
+++ R/as.R
@@ -119,3 +119,54 @@ as_AssayObject <- function(object) {
         return(CreateAssayObject(data = m))
     }
 }
+
+#' as_GiottoObject
+#'
+#' Convert Seurat Visium object to a GiottoObject
+
+#' @param object Seurat Object
+#' @param assay Seurat assay in \code{object}
+#' @param slot Seurat slot in \code{object} and \code{assay}
+#' @param ... Arguments passed to \code{Giotto::createGiottoObject}
+#' @export as_GiottoObject
+#' @examples
+#' # as_GiottoObject(object)
+as_GiottoObject <- function(object, assay = "Spatial", slot = "counts", ...) {
+    if (!is(object, "Seurat")) {
+        stop("Only Seurat objects supported")
+    }
+    if (!requireNamespace("Giotto", quietly = TRUE)) {
+        stop("Install Giotto.")
+    }
+    if (!requireNamespace("magick", quietly = TRUE)) {
+        stop("Install magick.")
+    }
+    if (length(Images(object)) > 1) {
+        stop("Currently only single sample objects supported.")
+    }
+    raw_matrix <- GetAssayData(object, assay = assay, slot = slot)
+    spatial_locs <- GetTissueCoordinates(object)
+    spatial_locs <- spatial_locs / object@images[[1]]@scale.factors$lowres
+    spatial_locs <-  spatial_locs[, c(2, 1)]
+    colnames(spatial_locs) <- c("sdimx", "sdimy")
+    spatial_locs$sdimy <- spatial_locs$sdimy * -1
+    cell_metadata <-  data.table(
+        cell_ID = rownames(object@images[[1]]@coordinates),
+        object@images[[1]]@coordinates[, c("tissue", "row", "col")])
+
+    colnames(cell_metadata) <- c("cell_ID", "in_tissue", "array_row", "array_col")
+    mg_object <- Giotto::createGiottoImage(spatial_locs = spatial_locs,
+        mg_object = magick::image_read(object@images[[1]]@image),
+        name = Images(object)[1],
+        scale_factor=object@images[[1]]@scale.factors$lowres)
+    images <- list(mg_object)
+    names(images) <- Images(object)[1]
+
+    Giotto::createGiottoObject(
+        expression = raw_matrix,
+        expression_feat = "rna",
+        spatial_locs = spatial_locs,
+        cell_metadata = list(cell = list(rna = cell_metadata)),
+        images = images,
+        ...)
+}    
